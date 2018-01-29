70ed025d6772d48230f1f00c38149ca85b618233
betsy18 <betsy_dessire@hotmail.com>
2018-01-29 02:48:25 -0500

    Actualizando readme

---
+++ src/path-to-cardify.js
@@ -0,0 +1,12 @@
+(function($) {
+  $.fn.showAltHover = function() {
+    $('img').hover(
+      function() {
+        $(this).css('display', 'none');
+        $('div').append($(this).attr('alt'));
+      });
+  };
+}(jQuery));
+
+// Usage example:
+$('div').showAltHover();
