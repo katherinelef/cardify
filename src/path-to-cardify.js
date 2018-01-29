2c8c50fae1e7fc691ef37dc9f08ebfd0217c8e37
Jennifercarmen <jennifercarmens@gmail.com>
2018-01-29 10:40:40 -0500

    añadiendo funcionalidad plugin

--- src/path-to-cardify.js
+++ src/path-to-cardify.js
@@ -1,9 +1,11 @@
 (function($) {
   $.fn.showAltHover = function() {
-    $('img').hover(
-      function() {
-        $(this).css('display', 'none');
-        $('div').append($(this).attr('alt'));
+
+    $("img").hover(function() {
+      console.log($(this).attr('alt'))
+      $(this).each(function() {
+        $(this).replaceWith("<figure>" + "<img src=" + $(this).attr("src")+ ">" + "<figcaption>"+ $(this).attr('alt') + "</figcaption>" + "</figure>");
+      });
       });
   };
 }(jQuery));
